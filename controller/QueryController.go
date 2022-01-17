2a0e82405df85d3788a719dcb257e58570d3895f
Sambo Chea <sombochea@cubetiqs.com>
2022-01-17 12:38:53 +0700

    Updated service checkup with health

--- controller/QueryController.go
+++ controller/QueryController.go
@@ -14,33 +14,32 @@ func QueryHandler(ctx *gin.Context) {
 
 	serviceJson, err := redis.HGet("services", serviceName)
 	if err != nil {
-		if redis.IsRedisNil(err){
-			ctx.JSON(http.StatusNotFound, model.NewApiErrorResponse(1004,"Service not found"))
+		if redis.IsRedisNil(err) {
+			ctx.JSON(http.StatusNotFound, model.NewApiErrorResponse(1004, "Service not found"))
 			return
 		}
-		ctx.JSON(http.StatusInternalServerError, model.NewApiErrorResponse(1005,"Failed to retrieve service details"))
+		ctx.JSON(http.StatusInternalServerError, model.NewApiErrorResponse(1005, "Failed to retrieve service details"))
 		return
 	}
 
 	serviceDef, err := model.FromJson(serviceJson)
-	if err != nil{
-		ctx.JSON(http.StatusInternalServerError, model.NewApiErrorResponse(1006,"Generic processing failure"))
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, model.NewApiErrorResponse(1006, "Generic processing failure"))
 		return
 	}
 	ctx.JSON(http.StatusOK, gin.H{
-		"status_code":"1000",
-		"status_description":"Success",
-		"body": serviceDef,
+		"status_code":        "1000",
+		"status_description": "Success",
+		"body":               serviceDef,
 	})
 }
-
 
 func QueryAllHandler(ctx *gin.Context) {
 
 	serviceMap, err := redis.HGetAll("services")
 	if err != nil {
-		if redis.IsRedisNil(err){
-			ctx.JSON(http.StatusNotFound, model.NewApiErrorResponse(1004,"No services found"))
+		if redis.IsRedisNil(err) {
+			ctx.JSON(http.StatusNotFound, model.NewApiErrorResponse(1004, "No services found"))
 			return
 		}
 		ctx.JSON(http.StatusInternalServerError, model.NewApiErrorResponse(1005, "Failed to retrieve service details"))
@@ -48,23 +47,23 @@ func QueryAllHandler(ctx *gin.Context) {
 	}
 
 	if len(serviceMap) == 0 {
-		ctx.JSON(http.StatusNotFound, model.NewApiErrorResponse(1004,"No services found"))
+		ctx.JSON(http.StatusNotFound, model.NewApiErrorResponse(1004, "No services found"))
 		return
 	}
 
 	newServiceMap := make(map[string]*model.ServiceDefinition)
-	for serviceName, serviceJson  := range serviceMap{
+	for serviceName, serviceJson := range serviceMap {
 		serviceDef, err := model.FromJson(serviceJson)
-		if err != nil{
-			ctx.JSON(http.StatusInternalServerError, model.NewApiErrorResponse(1006,"Generic processing failure"))
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, model.NewApiErrorResponse(1006, "Generic processing failure"))
 			return
 		}
 		newServiceMap[serviceName] = serviceDef
 	}
 
 	ctx.JSON(http.StatusOK, gin.H{
-		"status_code":"1000",
-		"status_description":"Success",
-		"body": newServiceMap,
+		"status_code":        "1000",
+		"status_description": "Success",
+		"body":               newServiceMap,
 	})
 }
